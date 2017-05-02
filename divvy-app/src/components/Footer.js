184d3cb2bc1eeedcc3010478146b16e10688e7b3
Samantha Keathley <samncollette@gmail.com>
2017-05-02 16:56:39 -0500

    more footer shenanigans

--- divvy-app/src/components/Footer.js
+++ divvy-app/src/components/Footer.js
@@ -1,32 +1,46 @@
 import React, {Component} from 'react';
 import {IndexLink, Link} from 'react-router';
 
-class NavBar extends Component {
+class Footer extends Component {
   render() {
     return (
 
-      <div className="container">
-        <div className="navbar-header">
-          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span className="sr-only">Toggle navigation</span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-          </button>
-
-          <nav className="tabs navbar navbar-default navbar-fixed-top">
-            <Link className="navbar-brand" to="/">divvy</Link>
-            <ul>
-              <li><IndexLink to="/" activeClassName="active" className="link">Home</IndexLink></li>
-              <li><Link to="/Goals" activeClassName="active" className="link">Goals</Link></li>
-              <li><Link to="/AddGoals" activeClassName="active" className="link">Add a New Goal</Link></li>
-              <li><Link to="/Profile" activeClassName="active" className="link">All about you</Link></li>
-            </ul>
-          </nav>
+    <div className="container">
+      <hr />
+        <div className="row">
+            <div className="col-md-3">
+              <ul className="nav nav-pills nav-stacked">
+                <li><h4>Our Goal</h4></li>
+                <li><p>is to educate yound adults and adolescent abou financial responsibility in a fun and engaging way.</p></li>
+              </ul>
+            </div>
+            <div className="col-md-3">
+              <ul className="nav nav-pills nav-stacked">
+                <li><h4>About</h4></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Our Team</a></li>
+                <li><a href="#">Careers</a></li>
+              </ul>
+            </div>
+            <div className="col-md-3">
+              <ul className="nav nav-pills nav-stacked">
+                <li><h4>Contact</h4></li>
+                <li><a href="#">Help Center</a></li>
+                <li><a href="#">Community</a></li>
+                <li><a href="#">Share Your Story</a></li>
+              </ul>
+            </div>
+            <div className="col-md-3">
+              <ul className="nav nav-pills nav-stacked">
+                <li><h4>Download Our App</h4></li>
+                <li><a href="#">iOS</a></li>
+                <li><a href="#">Android</a></li>
+              </ul>
+            </div>
+          </div>
         </div>
-      </div>
     );
   }
 }
 
-export default NavBar;
+export default Footer;

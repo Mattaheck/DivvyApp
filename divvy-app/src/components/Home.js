cb9738ccc1b907d8c3cf15669945d4d856c97931
crake <MATTATAT@Matthews-MacBook-Pro-3.local>
2017-05-02 12:55:37 -0500

    Continuing to build out Home.js & Home.css

--- divvy-app/src/components/Home.js
+++ divvy-app/src/components/Home.js
@@ -7,7 +7,7 @@ class Home extends Component {
       <div className="home container">
         <div className="homeContent container">
           <div className="row">
-            <div className="col-md-offset-2 col-sm-3 lT">
+            <div className="col-md-offset-2 col-md-3 col-sm-3 lT">
               <div className="lT1">
                 <span className="lT2">
                   Learn to Save<br/>
@@ -21,11 +21,33 @@ class Home extends Component {
         </div>
         <div className="aboutContent container">
           <div className="row">
-            <div className="col-md-offset-1">
-              <div className="col-md-12 aT">
-                About Content Goes Here
+            <div className="col-md-offset-1 col-md-6 aT">
+              Save Your Money
+            </div>
+          <div className="row">
+            <div className="col-md-offset-1 col-md-6">
+              <div className="aT1">
+                Earn points
               </div>
             </div>
+          </div>
+          <div className="row aboutRow">
+            <div className="col-md-offset-2 col-md-9 aBCol">
+              <div className="col-md-4 aB">
+
+              </div>
+              <div className="col-md-4 aB">
+
+              </div>
+              <div className="col-md-4 aB">
+
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            {/* <div className="col-md-offset-2 col-md-4 aB2">
+            </div> */}
+          </div>
           </div>
         </div>
       </div>

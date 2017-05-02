04d7d4adcda4a010c41d9f4d20ed172142995981
crake <MATTATAT@Matthews-MacBook-Pro-3.local>
2017-05-02 10:38:13 -0500

    Added html & css to App.js and Home.js

--- divvy-app/src/App.js
+++ divvy-app/src/App.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import {IndexLink} from 'react-router';
 import {Link} from 'react-router';
 import dotenv from 'dotenv';
-import './index.css';
+import './App.css';
+// import firebase from './utils/firebase';
 
 
 dotenv.config({silent:true});
@@ -10,24 +11,31 @@ dotenv.config({silent:true});
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="container">
 
-          <header className="navbar">
-            <h1 className="pull-left">divvy</h1>
-          </header>
+    <div className="App">
+      <div className="container">
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
 
-          <nav className="tabs">
+        <nav className="tabs navbar navbar-default navbar-fixed-top">
+          <Link className="navbar-brand" to="/">divvy</Link>
+          <ul>
             <li><IndexLink to="/" activeClassName="active" className="link">Home</IndexLink></li>
-            <li><Link to="/goals" activeClassName="active" className="link">Goals</Link></li>
+            <li><Link to="/Goals" activeClassName="active" className="link">Goals</Link></li>
             <li><Link to="/AddGoals" activeClassName="active" className="link">Add a New Goal</Link></li>
+          </ul>
           </nav>
-          <div className="content">
-            {this.props.children}
-          </div>
-
         </div>
       </div>
+      <div className="content">
+        {this.props.children}
+      </div>
+    </div>
     );
   }
 }
--- divvy-app/src/components/Home.js
+++ divvy-app/src/components/Home.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import './Home.css';
 
 class Home extends Component {
   render(){
     return(
-      <div>
-        <h2>Home and About content will go here</h2>
+      <div className="home container">
+        <div className="homeContent container">
+          <div className="row">
+            <div className="col-md-offset-2 col-sm-3 lT">
+              <div className="lT1">
+                <span className="lT2">
+                  Learn to Save<br/>
+                </span>
+                <span className="lT3">
+                  it's never been so fun!
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="aboutContent container">
+          <div className="row">
+            <div className="col-md-offset-1">
+              <div className="col-md-12 aT">
+                About Content Goes Here
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }

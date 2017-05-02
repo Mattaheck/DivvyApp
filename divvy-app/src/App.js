542928f85977bd8577ff834cd960cd401c5c708a
Samantha Keathley <samncollette@gmail.com>
2017-05-02 13:23:40 -0500

    moved navbar to its own component

--- divvy-app/src/App.js
+++ divvy-app/src/App.js
@@ -1,9 +1,7 @@
 import React, {Component} from 'react';
-import {IndexLink} from 'react-router';
-import {Link} from 'react-router';
 import dotenv from 'dotenv';
+import NavBar from './components/NavBar';
 import './App.css';
-import firebase from './utils/firebase';
 
 
 dotenv.config({silent:true});
@@ -13,26 +11,7 @@ class App extends Component {
     return (
 
     <div className="App">
-      <div className="container">
-        <div className="navbar-header">
-          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span className="sr-only">Toggle navigation</span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-          </button>
-
-          <nav className="tabs navbar navbar-default navbar-fixed-top">
-            <Link className="navbar-brand" to="/">divvy</Link>
-            <ul>
-              <li><IndexLink to="/" activeClassName="active" className="link">Home</IndexLink></li>
-              <li><Link to="/Goals" activeClassName="active" className="link">Goals</Link></li>
-              <li><Link to="/AddGoals" activeClassName="active" className="link">Add a New Goal</Link></li>
-              <li><Link to="/Profile" activeClassName="active" className="link">All about you</Link></li>
-            </ul>
-          </nav>
-        </div>
-      </div>
+      <NavBar />
       <div className="content">
         {this.props.children}
       </div>

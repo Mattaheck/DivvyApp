638d4f63e0e2e80ce078f71860556f38893403ba
Samantha Keathley <samncollette@gmail.com>
2017-05-03 14:20:01 -0500

    added progress bars

--- divvy-app/src/components/Profile.js
+++ divvy-app/src/components/Profile.js
@@ -32,7 +32,11 @@ class Profile extends Component {
           </div>
           <div className="col-md-offset-1 col-md-6 col-sm-3 rT">
             <span className="progressBar">
-              <p>Goal Progress Bars here</p>
+              <img src="http://piskel-imgstore-b.appspot.com/img/0a386ab3-3035-11e7-bb0e-13ef97b06e1a.gif" />
+              <h3>75% of the way there! Keep going!</h3>
+              <br/>
+              <img src="http://piskel-imgstore-b.appspot.com/img/0a386ab3-3035-11e7-bb0e-13ef97b06e1a.gif" />
+              <h3>90% of the way there!?! Astonishing!</h3>
             </span>
           </div>
         </div>

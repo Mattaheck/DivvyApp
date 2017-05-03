abc849b869951f8e45dc232eb62811f0e047f647
crake <MATTATAT@Matthews-MacBook-Pro-3.local>
2017-05-03 13:29:17 -0500

    Leftover conflicts

--- divvy-app/src/App.js
+++ divvy-app/src/App.js
@@ -46,58 +46,22 @@ class App extends Component {
   handleLogin(e){
   e.preventDefault();
   console.log('Login button clicked');
-<<<<<<< HEAD
 
   const provider = new firebase.auth.GithubAuthProvider();
   auth.signInWithPopup(provider);
-}
+  }
 
-handleLogout(e){
-  e.preventDefault();
-  console.log('Logout button clicked');
-  auth.signOut();
-}
+  handleLogout(e){
+    e.preventDefault();
+    console.log('Logout button clicked');
+    auth.signOut();
+  }
 
-=======
-
-  const provider = new firebase.auth.GithubAuthProvider();
-  auth.signInWithPopup(provider);
-}
-
-handleLogout(e){
-  e.preventDefault();
-  console.log('Logout button clicked');
-  auth.signOut();
-}
-
-//   firebase.auth().signInWithPopup(provider).then(function(result) {
-//   // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-//   var token = result.credential.accessToken;
-//   // The signed-in user info.
-//   var user = result.user;
-//   // ...
-// }).catch(function(error) {
-//   // Handle Errors here.
-//   var errorCode = error.code;
-//   var errorMessage = error.message;
-//   // The email of the user's account used.
-//   var email = error.email;
-//   // The firebase.auth.AuthCredential type that was used.
-//   var credential = error.credential;
-//   // ...
-// });
-
-// <div className="content">
-// currentUser= { this.state.currentUser }
-// loginButtonClicked={ this.loginButtonClicked }
-// logoutButtonClicked={ this.logoutButtonClicked } />
->>>>>>> mattheck_branch
   logoutButtonClicked(e) {
     e.preventDefault();
 
     auth.signOut();
   }
-
 
   render() {
     return (

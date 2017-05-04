9f24e14853b58483e07e28e129794580be92d1ee
Samantha Keathley <samncollette@gmail.com>
2017-05-03 22:01:39 -0500

    added dwarf

--- divvy-app/src/components/Avatar.js
+++ divvy-app/src/components/Avatar.js
@@ -3,9 +3,10 @@ import './Avatar.css';
 
 const randomArrayElement = arr => arr[Math.floor(Math.random() * arr.length)];
 const avatars = [
-  'http://piskel-imgstore-b.appspot.com/img/d89a96eb-3027-11e7-92c5-13ef97b06e1a.gif',
-  'http://piskel-imgstore-b.appspot.com/img/aab9953d-3033-11e7-9568-13ef97b06e1a.gif',
-  'http://piskel-imgstore-b.appspot.com/img/f9da5b70-3041-11e7-9a1f-13ef97b06e1a.gif',
+  'http://piskel-imgstore-b.appspot.com/img/5d11a9b8-3075-11e7-8664-7378e3ca4582.gif',
+  'http://piskel-imgstore-b.appspot.com/img/85627973-3075-11e7-b249-7378e3ca4582.gif',
+  'http://piskel-imgstore-b.appspot.com/img/afb4e52b-3075-11e7-aea4-7378e3ca4582.gif',
+  'http://piskel-imgstore-b.appspot.com/img/05fc5a38-3075-11e7-9b60-7378e3ca4582.gif',
 ];
 
 class Avatar extends Component {

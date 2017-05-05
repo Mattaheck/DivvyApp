cfb52322bd8dbd115341c4bbe92dc2f5a175cd2a
Samantha Keathley <samncollette@gmail.com>
2017-05-05 00:18:47 -0500

    Fix user login

--- divvy-app/src/App.js
+++ divvy-app/src/App.js
@@ -1,16 +1,16 @@
 import React, {Component} from 'react';
 import dotenv from 'dotenv';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router';
+// import {
+//   BrowserRouter as Router,
+//   Route
+// } from 'react-router';
 
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
-import Home from './components/Home';
-import Profile from './components/Profile';
+// import Home from './components/Home';
+// import Profile from './components/Profile';
 import './App.css';
-import { firebase, auth, database, firebaseListToArray } from './utils/firebase';
+import { firebase, auth, database/*, firebaseListToArray*/ } from './utils/firebase';
 
 dotenv.config({silent:true});
 
@@ -19,60 +19,53 @@ class App extends Component {
     super(props);
 
     this.state = {
-      currentUser: ''
-    }; this.ref = database.ref('/goals')
+      currentUser: null
+    };
+    this.ref = database.ref('/goals')
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
   }
 
   componentWillMount() {
-    database.ref('/users')
-    .on('value', data => {
-      const results = firebaseListToArray(data.val());
-      console.log('Users', results);
-
-      this.setState({ currentUser:results[0] });
-      console.log(this.state.currentUser.id)
-
-    });
-  }
-
-
-handleLogin(e){
-  // e.preventDefault();
-  console.log('Login button clicked');
-
-  const provider = new firebase.auth.GoogleAuthProvider();
-  auth.signInWithPopup(provider).then(function(result){
-
-      console.log('Fuck yeah. Result: ', result);
-
-      database.ref('/user').push({
-        users: this.currentUser.displayName
+    if (auth.currentUser){
+      this.setState({
+        currentUser: auth.currentUser
       })
-      }).catch(function(error){
-      console.log('Error: ', error);
-    })
+    }
   }
 
+  handleLogin(e){
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider)
+      .then(result => {
+        this.setState({
+          currentUser: result.user
+        })
+      })
+      .catch(function(error){
+        console.log('Error: ', error);
+      })
+  }
 
-handleLogout(e){
-  e.preventDefault();
-  console.log('Logout button clicked');
-  auth.signOut();
-}
-
+  handleLogout(e){
+    auth.signOut()
+      .then(() => {
+        this.setState({
+          currentUser: null
+        })
+      });
+  }
 
   render() {
     return (
-    <div className="App">
-      <NavBar
-      handleLogout={ this.handleLogout }
-      handleLogin={ this.handleLogin }
-      currentUser={ this.state.currentUser }/>
-      <div className="content">
-        {this.props.children}
-        <p>Hello, {this.state.currentUser.id}</p>
-        {this.state.currentUser.goals}
-      </div>
+      <div className="App">
+        <NavBar
+        handleLogout={ this.handleLogout }
+        handleLogin={ this.handleLogin }
+        currentUser={ this.state.currentUser }/>
+        <div className="content">
+          {this.props.children}
+        </div>
       <Footer />
       </div>
     );
